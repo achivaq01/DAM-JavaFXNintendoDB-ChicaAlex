a3c0db4b6eae64088249214085d16bc07f958f41
chica_alex_gitthub@gmx.es <chica_alex_gitthub@gmx.es>
2023-10-08 11:43:42 +0200

    se ha intentado

--- Project/src/ControllerMobile0.java
+++ Project/src/ControllerMobile0.java
@@ -1,3 +1,29 @@
-public class ControllerMobile0 {
-    
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerMobile0 implements Initializable {
+    String[] opcions = { "Personatges", "Jocs", "Consoles" };
+
+    @FXML
+    private ListView<Label> list = new ListView<>();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Label personatges = new Label(opcions[0]);
+        Label jocs = new Label(opcions[1]);
+        Label consoles = new Label(opcions[2]);
+
+        list.getItems().add(personatges);
+        list.getItems().add(jocs);
+        list.getItems().add(consoles);
+
+
+
+    }
+
+
 }
